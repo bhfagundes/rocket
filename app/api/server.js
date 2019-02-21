81844406737c345a333d2490944fc6b7611f81b6
Brenner Fagundes <bhfagundes@gmail.com>
2019-02-21 00:01:37 -0300

    trabalhando com imagens

--- app/api/server.js
+++ app/api/server.js
@@ -1,13 +1,16 @@
 var express = require('express'),
     bodyParser = require('body-parser'),
+    multiparty= require('connect-multiparty'),
     mongodb = require('mongodb'),
-    objectId = require('mongodb').ObjectID;
+    objectId = require('mongodb').ObjectID,
+    fs = require('fs');
 
 var app = express();
 
 // body-parser
 app.use(bodyParser.urlencoded({extended:true}));
 app.use(bodyParser.json());
+app.use(multiparty());
 
 var port = 82;
 
@@ -27,7 +30,26 @@ app.get('/', function(req, res){
 
 // POST(create)
 app.post('/api', function(req, res){
+    res.setHeader("Access-Control-Allow-Origin","*");
     var dados = req.body;
+    var date = new Date();
+    time_stamp = date.getTime();
+    var url_imagem = time_stamp + "_" + req.files.arquivo.originalFilename;
+    var path_origem = req.files.arquivo.path;
+    var path_destino = './uploads/' + url_imagem;
+    fs.rename(path_origem, path_destino, function(err){
+        if(err)
+        {
+            res.status(500).json({error:err});
+            return;
+        }
+    });
+    var dados = {
+        url_imagem: url_imagem,
+        titulo: req.body.titulo
+    };
+
+   
     db.open(function(err, mongoclient){
         mongoclient.collection('postagens', function(err, collection){
             collection.insert(dados, function(err, records){
@@ -44,6 +66,7 @@ app.post('/api', function(req, res){
 
 //get
 app.get('/api', function(req, res){
+    res.setHeader("Access-Control-Allow-Origin","*");
     db.open(function(err, mongoclient){
         mongoclient.collection('postagens', function(err, collection){
             collection.find().toArray(function(err,results){
@@ -58,6 +81,20 @@ app.get('/api', function(req, res){
                 mongoclient.close();
             });
         });
+    });
+});
+
+app.get('/uploads/:imagem',function(req,res){
+    var img = req.params.imagem;
+
+    fs.readFile('./uploads/'+img,function(err,content){
+        if(err)
+        {
+            res.status(400).json(err);
+            return;
+        }
+        res.writeHead(200, {'Content-type':'image/jpg'});
+        res.end(content);
     });
 });
 // GET by ID(ready)
